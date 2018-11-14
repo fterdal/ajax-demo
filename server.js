47699f5a96c9a7ffdc05d2ad140dbca61decc6b5
Finn Terdal <fterdal@gmail.com>
2018-11-14 13:37:14 -0600

    finished lecture

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ const app = express()
 app.use(volleyball)
 
 app.get('/robots', (req, res, next) => {
+  // console.log('HELLO IM THE SERVER')
   const robotNames = [
     'Brett', 'Finn', 'Priti',
-    'Collin', 'Ben', 'YOUR NAME HERE'
+    'Collin', 'Ben', 'YOUR_NAME_HERE'
   ]
   res.json(robotNames.map(robot => ({
     name: robot,
